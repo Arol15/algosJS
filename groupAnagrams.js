8bacf1c2e28ff0eedfff665b5ea8d4a120e3bcc5
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-10-06 19:04:12 -0400

    final solution for groupAnagrams

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,16 +1,19 @@
 function groupAnagrams(strs) {
   const obj = {};
 
-  for (let word of strs) {
-    let sortedWord = word.split("").sort().join("");
-    if (!obj[sortedWord]) {
-      obj[sortedWord] = [];
-    }
-  }
+  // for (let word of strs) {
+  //   let sortedWord = word.split("").sort().join("");
+  //   if (!obj[sortedWord]) {
+  //     obj[sortedWord] = [];
+  //   }
+  // }
 
   for (let word of strs) {
-    if (obj[word.split("").sort().join("")]) {
-      obj[word.split("").sort().join("")].push(word);
+    let sortedWord = word.split("").sort().join("");
+    if (obj[sortedWord]) {
+      obj[sortedWord].push(word);
+    } else {
+      obj[sortedWord] = [word];
     }
   }
 
