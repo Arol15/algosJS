2183e2679dde15b96e9c3af363ea18258af8c5ee
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-10-12 17:00:03 -0400

    buy and sell stock easy first attempt using two pointers

---
+++ buySellStockEasy.js
@@ -0,0 +1,22 @@
+// leetocde Best time to Buy and Sell Stock #121
+function maxProfit(prices) {
+  let maxProfit = 0;
+  let leftPointer = 0;
+  let rightPointer = 1;
+
+  while (rightPointer < prices.length) {
+    if (prices[rightPointer] > prices[leftPointer]) {
+      maxProfit = prices[rightPointer] - prices[leftPointer];
+      // console.log(maxProfit);
+      rightPointer++;
+    } else {
+      rightPointer++;
+      leftPointer++;
+    }
+  }
+
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); //4
+console.log(maxProfit([7, 6, 4, 3, 1])); //0
