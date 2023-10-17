4322ebd9975a4dd0eba957258c86c307fc240c54
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-10-17 14:14:14 -0400

    add edge cases

--- removeKDigits.js
+++ removeKDigits.js
@@ -1,5 +1,5 @@
 function removeKDigits(num, digits) {
-  const stack = []; //4 6
+  const stack = []; //12324
 
   for (let n of num) {
     while (digits > 0 && stack.length > 0 && n < stack[stack.length - 1]) {
@@ -14,7 +14,8 @@ function removeKDigits(num, digits) {
     digits--;
   }
 
-  return stack.join("");
+  let result = stack.join("");
+  return result ? String(Number(result)) : "0";
 }
 
 console.log(removeKDigits("462839", 2)); //2839
@@ -22,3 +23,5 @@ console.log(removeKDigits("462839", 3)); //239
 console.log(removeKDigits("543210", 2)); //3210
 console.log(removeKDigits("543210", 4)); //10
 console.log(removeKDigits("123456", 4)); //12
+console.log(removeKDigits("10200", 1)); //200
+console.log(removeKDigits("010", 1)); //0
