e378e38e3cb24583f0952fefed1c429e7dd513c3
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-10-17 14:18:52 -0400

    add prompt for mergeoverlapping intervals

---
+++ overlappingIntervals.js
@@ -0,0 +1,11 @@
+function mergeOverlappingIntervals(intervals) {}
+
+console.log(
+  mergeOverlappingIntervals([
+    [1, 2],
+    [3, 5],
+    [4, 7],
+    [6, 8],
+    [9, 10],
+  ])
+); //[[1,2], [3, 8], [9, 10]]
